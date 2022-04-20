caaf58fe6d2693a180ea5cdb81ed946d3a0c27d4
zhaoyuanhao <zhaoyuanhao@bjxzkjyxgs14.wecom.work>
2022-04-20 23:37:32 +0800

    [init]初始化提交

---
+++ src/main/java/com/zyh/rocket/aliyun/core/consumer/AliyunBatchConsumerListener.java
@@ -0,0 +1,13 @@
+package com.zyh.rocket.aliyun.core.consumer;
+
+import com.aliyun.openservices.ons.api.Action;
+import com.zyh.rocket.base.core.consumer.RocketBatchConsumerListener;
+
+/**
+ * 阿里云SDK 批处理消费监听器
+ *
+ * @author 赵元昊
+ * @date 2021/08/19 13:38
+ **/
+public interface AliyunBatchConsumerListener<T> extends RocketBatchConsumerListener<T, Action> {
+}
